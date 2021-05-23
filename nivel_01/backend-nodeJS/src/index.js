8dde348014183ff8433356d595e0b8c6051e16d0
Igor Martins <igormaceio.al@gmail.com>
2021-05-23 13:17:57 -0300

    Aplicação Funcional

--- nivel_01/backend-nodeJS/src/index.js
+++ nivel_01/backend-nodeJS/src/index.js
@@ -1,13 +1,74 @@
-const express = require('express');
+const express = require('express')
+const { uuid } = require('uuidv4')
 
-const app = express();
+const app = express()
+app.use(express.json())
 
-app.get('/', (request, response) => {
+const projects = []
 
-  return response.json({message: 'Hello GoStack and World'})
+app.get('/projects', (request, response) => {
+
+  const { title } = request.query
+
+  const results = title 
+  ? projects.filter(project => project.title.includes(title))
+  : projects //função ternária
+
+  return response.json(results)
   
 });
 
+app.post('/projects', (request, response) => {
+
+  const { title, owner } = request.body
+  const project = { id: uuid(), title, owner }
+
+  projects.push(project)
+  
+  return response.json(project)
+
+})
+
+app.put('/projects/:id', (request, response) => {
+
+  const { id } = request.params
+  const { title, owner } = request.body
+  const projectIndex = projects.findIndex(project => project.id === id)
+
+  if(projectIndex < 0) {
+    return response.status(400).json({ error: "Project not found"})
+  }
+
+  const project = {
+    id,
+    title,
+    owner
+  }
+
+  projects[projectIndex] = project
+
+  return response.json(project)
+
+})
+
+app.delete('/projects/:id', (request, response) => {
+  
+  const { id } = request.params
+  
+  const projectIndex = projects.findIndex(project => project.id === id)
+
+  if(projectIndex < 0) {
+    return response.status(400).json({ error: "Project not found"})
+  }
+
+  projects.splice(projectIndex, 1)
+
+  return response.status(204).send()
+
+})
+
 app.listen(3333, () => {
+
   console.log('🚀 Back-end Started!')
+
 });
